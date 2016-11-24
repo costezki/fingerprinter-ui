72b4e26c6ed3d664ce94e5845da7f86e21c3ca35
lordgarod <wizgod@mail.ru>
2016-11-24 19:10:13 +0200

    Added local authentication system by passport library

--- app.js
+++ app.js
@@ -5,6 +5,19 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var mkdirp = require('mkdirp');
 
+var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb://localhost/fingerprinter');
+var db = mongoose.connection;
+
 // var bodyParser = require('body-parser');
 
 // var PythonShell = require('python-shell');
@@ -13,13 +26,13 @@ var child_process = require('child_process');
 // var formidable = require('express-formidable');
 
 //
-var path = require('path');
 var formidable = require('formidable');
 var fs = require('fs');
 //
 var index = require('./routes/index');
 var stats = require('./routes/stats-form');
 var diff = require('./routes/diff-form');
+var users = require('./routes/users');
 
 var app = express();
 
@@ -29,8 +42,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -38,9 +51,51 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'reports')));
 
+// Express Session
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+// Passport init
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Express Validator
+app.use(expressValidator({
+    errorFormatter: function (param, msg, value) {
+        var namespace = param.split('.')
+            , root = namespace.shift()
+            , formParam = root;
+
+        while (namespace.length) {
+            formParam += '[' + namespace.shift() + ']';
+        }
+        return {
+            param: formParam,
+            msg: msg,
+            value: value
+        };
+    }
+}));
+
+// Connect Flash
+app.use(flash());
+
+// Global Vars
+app.use(function (req, res, next) {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    res.locals.user = req.user || null;
+    next();
+});
+
 app.use('/', index);
 app.use('/', stats);
 app.use('/', diff);
+app.use('/users', users);
 
 // solution of exposing static folders
 // http://stackoverflow.com/questions/27464168/how-to-include-scripts-located-inside-the-node-modules-folder
--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,15 @@ router.get('/', function (req, res, next) {
     res.render('index', {title: 'RDF Fingerprinter'});
 });
 
+function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    } else {
+        //req.flash('error_msg','You are not logged in');
+        res.redirect('/users/login');
+    }
+}
+
 module.exports = router;
 
 router.get("/reports/:filename", function (req, res) {
