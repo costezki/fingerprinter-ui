833e3ad1b47bbcb4cef3d01a6e0517fbe3dbf90b
lps <costezki.eugen@gmail.com>
2017-04-10 18:42:56 +0200

    updated project configuration

--- pysrc/import-script.sh
+++ pysrc/import-script.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 rm -rf RDF-fingerprint-diff
-git clone https://github.com/costezki/RDF-fingerprint-diff.git && cd ./RDF-fingerprint-diff && pip install .
+git clone https://github.com/costezki/RDF-fingerprint-diff.git && cd ./RDF-fingerprint-diff && sudo pip install .
